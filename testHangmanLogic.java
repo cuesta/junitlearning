f0c6d2a7210a7c86d5bbf755a3dac4ca9c1d999f
Carlos Cuesta <carloscuesta@gmail.com>
2013-06-01 21:02:41 -0600

    Update testHangmanLogic.java

--- testHangmanLogic.java
+++ testHangmanLogic.java
@@ -17,17 +17,38 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class testHangmanLogic {
 	char t = 't';
 	private static String keyPhrase = "test";
 	private int numberOfGuesses = 5;
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
 
 	@Test
-	public void testGuessCharacter() throws InvalidInputException, AlreadyGuessedException, InvalidConfigurationException {
-		HangmanLogic hg = new HangmanLogic(keyPhrase, 10);
-		Assert.assertEquals(true, HangmanLogic.guessCharacter(t));
+	public void testHangmanLogicInvalidPhrase() throws InvalidConfigurationException  {
+		HangmanLogic hglt = new HangmanLogic(" ", 10);
+	    exception.expect(InvalidConfigurationException.class);
+	    
 	}
+	
+	
+	@Test
+	public void testGuessCharacter() throws InvalidInputException, AlreadyGuessedException, InvalidConfigurationException {
+		HangmanLogic hggc = new HangmanLogic(keyPhrase, 10);
+		Assert.assertEquals(true, hggc.guessCharacter(t));
+	}
+	
 
+	 @Test
+	  public void doStuffThrowsIndexOutOfBoundsException() throws InvalidConfigurationException, InvalidInputException, AlreadyGuessedException {
+		HangmanLogic hgkp = new HangmanLogic(keyPhrase, 10);
 
+	    exception.expect(InvalidInputException.class);
+		Assert.assertEquals(true, hgkp.guessCharacter('8'));
+	 }
+	 
 }
